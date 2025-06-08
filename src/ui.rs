b1a0aa52694164eb77ac0746f8e8ed6106c69a22
Erik Ring-Walters <erik.ring.walters@gmail.com>
2025-06-08 15:01:18 -0700

    Centered win text

--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_simple_subsecond_system::hot;
 
 use crate::level::*;
 
@@ -9,41 +10,57 @@ pub struct WinText;
 
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, display_instructions)
+        app.add_systems(Startup, (spawn_instructions, spawn_win_text))
             .add_systems(Update, display_win);
     }
 }
 
-fn display_instructions(mut commands: Commands) {
+#[hot]
+fn spawn_instructions(mut commands: Commands) {
     commands.spawn((
         Text::new(
-            "Click to add dominoes.\n\nSpace to start physics.\n\nR or Z to undo.\n\nC to clear all dominoes.",
+            "Left-Click to add dominoes.\n\nRight-Click or Middle Mouse to orbit.\n\nSpace to start physics.\n\nR or Z to undo.\n\nC to clear all.",
         ),
         Node {
             position_type: PositionType::Absolute,
             top: Val::Px(12.0),
             left: Val::Px(12.0),
             ..default()
-        },   
-    ));     
+        },
+    ));
+}
+
+#[hot]
+fn spawn_win_text(mut commands: Commands) {
     commands.spawn((
-        Text::new("You Win!"),
         WinText,
+        Text::new("You Win!"),
+        TextFont {
+            font_size: 100.0,
+            ..default()
+        },
+        TextLayout::new_with_justify(JustifyText::Center),
         TextColor(Color::srgb(0., 0.8, 0.8)),
-        Visibility::Hidden,
+        Visibility::Visible,
         Node {
-            position_type: PositionType::Absolute,
+            // position_type: PositionType::Relative,
             top: Val::Percent(15.0),
-            left: Val::Percent(50.0),
+            left: Val::Percent(0.0),
+            // right: Val::Percent(0.0),
+            // align_self: AlignSelf::Center,
+            justify_content: JustifyContent::Center,
+            justify_items: JustifyItems::Center,
+            justify_self: JustifySelf::Center,
             ..default()
         },
     ));
 }
 
+#[hot]
 fn display_win(level: Res<Level>, mut vis: Single<&mut Visibility, With<WinText>>) {
-    if level.is_won {
-        **vis = Visibility::Visible;
+    **vis = if level.is_won {
+        Visibility::Visible
     } else {
-        **vis = Visibility::Hidden;
+        Visibility::Hidden
     }
 }
